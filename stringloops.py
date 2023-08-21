2bce0452c5059ee9dc42e04030352adbf07a506b
Shiven1907 <shefumalik@gmail.com>
2023-08-21 20:39:44 +0530

    advancded level ka programme

--- stringloops.py
+++ stringloops.py
@@ -1,9 +1,22 @@
-w="Helloji"
-print(w[-3:0:-1])
-
-"""
--7  -6   -5   -4   -3   -2   -1
-H    E    L    L    O    J    I
-0    1    2    3    4    5    6
-"""
-
+word= input("enter a sentence: ")
+count_vowels=0
+count_consonents=0
+count_num=0
+count_spaces=0
+count_sp=0
+for things in word :
+     if things == ' ':
+          count_spaces += 1
+     if things in '0123456789':
+          count_num += 1
+     if things in 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ':
+          count_consonents += 1
+     if things in 'aAeEiIoOuU':
+          count_vowels += 1
+     if things in """!@#$%^&*()-_=+'":;>.<,/?\|`~""":
+          count_sp += 1
+print("spaces are: ",count_spaces)
+print("numbers are: ",count_num)
+print("consonents are: ",count_consonents)
+print("vowels are: ", count_vowels)   
+print("special characters are: ", count_sp)  
